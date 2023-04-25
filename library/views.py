e190ca78aedd108b7675c435ae5145bf8d08474c
Mindaugas Ivanauskas <mindaugas.ivanauskas@gmail.com>
2023-04-25 22:06:03 +0300

    add index.html, base.html and static routing

--- library/views.py
+++ library/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Genre, Author, Book, BookInstance
+
 
 def index(request):
-    return HttpResponse('Hello world!')
+    book_count = Book.objects.all().count()
+    book_instance_count = BookInstance.objects.all().count()
+    aveilable_books_count = BookInstance.objects.filter(book_status__exact='a').count()
 
+    author_count = Author.objects.all().count()
+
+    context = {
+        'books': book_count,
+        'book_instances': book_instance_count,
+        'authors': author_count,
+        'available': aveilable_books_count
+    }
+
+    return render(request, "index.html", context)
--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 from decouple import config
 
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
