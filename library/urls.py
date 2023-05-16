6b41800d3a64e3f78efa33a95b9601e236583d89
Mindaugas Ivanauskas <mindaugas.ivanauskas@gmail.com>
2023-05-16 11:31:27 +0300

    not finished profile...

--- library/urls.py
+++ library/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('books/', views.BookListView.as_view(), name='books'),
     path('books/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('search/', views.search, name='search'),
+    path('profile/', views.profile, name='profile'),
 ]
